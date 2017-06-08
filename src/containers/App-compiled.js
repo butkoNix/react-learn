85ce6be2ed11c43f1ca8a85d0988b5b9afd153b2
butko <MaksimButko@nixsolutions.com>
2017-06-08 19:05:51 +0300

    Init commit of Redux learn

---
+++ src/containers/App-compiled.js
@@ -0,0 +1,3 @@
+"use strict";
+
+//# sourceMappingURL=App-compiled.js.map
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore( () => {}, {}) //WAT ;)
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
